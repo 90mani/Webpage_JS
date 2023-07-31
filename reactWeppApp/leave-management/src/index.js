d179b21334501f0e9a1240c640245f0396e9c3d8
Chinnasamy, Manikandan <Manikandan.Chinnasamy@nielseniq.com>
2023-07-31 11:12:18 +0530

    sample code for multiple files in react js

--- reactWeppApp/leave-management/src/index.js
+++ reactWeppApp/leave-management/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '../src/sample/firstReactTest';
+import Home from './pages/home'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
